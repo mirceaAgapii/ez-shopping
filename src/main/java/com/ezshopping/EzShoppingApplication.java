2aeca03038b9c4bbcc5032cde90af9376fe5c5fa
Mircea Agapii <mircea9000@gmail.com>
2021-10-26 20:58:34 +0300

    Add Spring Boot default project

---
+++ src/main/java/com/ezshopping/EzShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.ezshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezshopping/EzShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
